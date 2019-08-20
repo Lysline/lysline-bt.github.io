28fafda878ec7f0e74b0cf25ad0ba13378c81c24
Lysline <50519206+Lysline@users.noreply.github.com>
2019-08-21 01:12:36 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
-<noscript>
-
-</noscript>
+<script>
+var buttonstate=1;
+function onoff(element)
+{
+  buttonstate= buttonstate - 1;
+  var blabel;
+  if(buttonstate)
+  {
+    blabel="on";
+  }
+  else
+  {
+    blabel="off";
+  }
+  var child=element.firstChild;
+  child.innerHTML=blabel;
+}
+</script>
