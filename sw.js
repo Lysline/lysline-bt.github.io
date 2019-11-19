c72c7500e41000b2de0aed09bbc6ec94f82571b6
Lysline <50519206+Lysline@users.noreply.github.com>
2019-11-19 23:56:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    
     if('serviceWorker' in navigator){
 	    window.addEventListener('load', () => {
-                navigator.serviceWorker.register('../serviceworker.js')
+                navigator.serviceWorker.register('serviceworker.js')
                                        .then(reg => console.log('Service Worker registered'))
                                        .catch(err => console.log('Service Worker registration failed'))
 	    });
