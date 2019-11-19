f111df21bf2c007ecce0a46e4ecdb8cd41663df9
Lysline <50519206+Lysline@users.noreply.github.com>
2019-11-19 23:31:41 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,13 @@
 /* https://www.youtube.com/watch?v=ksXwaWHCW6k*/
 
-const cacheName ='me';
+const cacheName ='new';
 
 const cacheAssets=[
+  'index.html',
   'styles.css',
   'app.js'
 ];
+// Install Event
 self.addEventListener('install', (e) =>{
   console.log('Service Worker: Installed');
 e.waitUntil(
@@ -18,6 +20,7 @@ e.waitUntil(
     .then(() => self.skipWaiting())
   );
 });
+// Activate Event
 self.addEventListener('activate', e => {
   console.log('Service Worker: Activated');
   // Remove unwanted caches
@@ -32,21 +35,10 @@ self.addEventListener('activate', e => {
     })
   ); 
 });
-//https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
-// Service Worker make the page offline accessible 
+// Fetch Event 
 
 self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
+    console.log('Service Worker: Fetching');
    e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  ); 
+     fetch(e.request).catch() => caches.match(e.request)));
 });
