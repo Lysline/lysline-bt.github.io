891c10faad35035e2e7a2b37d10a6d333afe16fd
Lysline <50519206+Lysline@users.noreply.github.com>
2019-11-20 00:08:27 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -42,5 +42,5 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', (e) => {
     console.log('Service Worker: Fetching');
    e.respondWith(
-     fetch(e.request).catch() => caches.match(e.request)));
+     fetch(e.request).catch(() => caches.match(e.request)));
 });
